70add70b9c4ab0e77c385667fdeedacc0de641a0
mmmatheus <116230705+matheus676@users.noreply.github.com>
2022-10-20 00:53:48 +0000

    Aprendi a criar variáveis e exibi-las na tela!

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #Meu primeiro projeto Python!!!
 #
 #print() = comande de saida
-
-
+#Quando quiser guardar uma String!
+nome="Matheus Martinelli"
+#Quando quiser guardar um numero inteiro
+idade=18
 print("Hello World!")
+#Exibir o meu nome(que está dentro da variável nome)
+print(nome)
 '''
 Comentario em bloco
 
